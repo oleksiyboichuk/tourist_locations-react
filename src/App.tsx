b22d344a546d701242357c52e2f9620df17a2359
Oleksiy Boichuk <oleskiy.boichuk.v@gmail.com>
2024-12-31 19:30:53 +0200

    install headless ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Wrapper from "./components/wrapper";
+import { Button } from "@headlessui/react";
 
 function App() {
   return (
     <Wrapper>
-      <div>Text</div>
+      <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
+        Save changes
+      </Button>
     </Wrapper>
   );
 }
