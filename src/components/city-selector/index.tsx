c1017ee2b4189586732a29ddd41b1a977db0667f
_boy4uk <oleskiy.boichuk.v@gmail.com>
2025-01-06 10:56:45 +0200

    update params

--- src/components/city-selector/index.tsx
+++ src/components/city-selector/index.tsx
@@ -1,22 +1,48 @@
-import { Select } from '@headlessui/react'
-import {FC, useEffect} from "react";
+import { Select } from '@headlessui/react';
+import React, { FC, useEffect, useState } from "react";
+import { getCityList } from "../../services/tourist-location.service.ts";
+import { CityListModel } from "../../models/location.model.ts";
 
 interface CitySelectorProps {
-
+    onCitySelect: (selectedCity: string) => void;
 }
 
-const CitySelector:FC<CitySelectorProps> = () => {
+const CitySelector: FC<CitySelectorProps> = ({ onCitySelect }) => {
+    const [cities, setCities] = useState<CityListModel[]>([]);
+    const [selectedCity, setSelectedCity] = useState<string>("");
 
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const cities = await getCityList();
+                setCities(cities);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
+        fetchData();
     }, []);
 
-    return(
-        <Select name="status" aria-label="Project status">
-            <option value="active">Active</option>
-            <option value="paused">Paused</option>
-            <option value="delayed">Delayed</option>
-            <option value="canceled">Canceled</option>
+    const handleCityChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const city = event.target.value;
+        setSelectedCity(city);
+        onCitySelect(city);
+    };
+
+    return (
+        <Select
+            name="city-selector"
+            aria-label="Select a city"
+            value={selectedCity}
+            onChange={handleCityChange}
+        >
+            <option value="" disabled>Select a city</option>
+            {cities.map((city: CityListModel) => (
+                <option key={city.CityName} value={city.CityName}>
+                    {city.CityName}
+                </option>
+            ))}
         </Select>
     );
 }
