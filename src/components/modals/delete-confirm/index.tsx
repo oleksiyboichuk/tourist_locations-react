87a8abf4199f69589f529d558d7358c0c0a5895a
_boy4uk <oleskiy.boichuk.v@gmail.com>
2025-01-17 09:52:35 +0200

    update delete logic

--- src/components/modals/delete-confirm/index.tsx
+++ src/components/modals/delete-confirm/index.tsx
@@ -1,4 +1,5 @@
-import {Button} from "@headlessui/react";
+import { useEffect, useState } from "react";
+import { Button } from "@headlessui/react";
 
 const DeleteConfirmModal = ({
                                 message,
@@ -7,25 +8,49 @@ const DeleteConfirmModal = ({
     message: string;
     onClose: (confirmed: boolean) => void;
 }) => {
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        setVisible(true);
+    }, []);
+
+    const handleClose = (confirmed: boolean) => {
+        setVisible(false);
+
+        setTimeout(() => {
+            onClose(confirmed);
+        }, 200);
+    };
+
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center">
             <div
-                className="absolute inset-0 bg-black/60"
-                onClick={() => onClose(false)}
+                className={`
+          absolute inset-0 bg-black
+          transition-opacity duration-300
+          ${visible ? "opacity-60" : "opacity-0"}
+        `}
+                onClick={() => handleClose(false)}
             ></div>
 
-            <div className="relative bg-neutral-900 py-5 px-7 border-[1px] border-white/5 rounded shadow-lg z-10">
+            <div
+                className={`
+          relative bg-neutral-900 py-5 px-7 border-[1px] border-white/5 rounded shadow-lg z-10
+          transition-opacity duration-300
+          ${visible ? "opacity-100" : "opacity-0"}
+        `}
+            >
                 <p className="mb-4 text-white">{message}</p>
                 <div className="flex justify-end items-center gap-3">
                     <Button
                         className="bg-green-800 text-white px-3 py-1 rounded transition-colors hover:bg-green-700/90"
-                        onClick={() => onClose(true)}
+                        onClick={() => handleClose(true)}
                     >
                         Так
                     </Button>
                     <Button
                         className="bg-rose-700 text-white px-4 py-1 rounded transition-colors hover:bg-rose-600/90"
-                        onClick={() => onClose(false)}
+                        onClick={() => handleClose(false)}
                     >
                         Закрити
                     </Button>
